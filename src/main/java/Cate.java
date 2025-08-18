cdf5b25a6d1421e475337a9dca7ffd0e64daf894
benlua <benlua73@gmail.com>
2025-08-19 00:06:09 +0800

    Implement Level 0 chatbot with rename, greet, and exit
    Renamed Duke to Cate and removed all references to the old name. Added a
    skeletal version that greets the user on start and exits gracefully. This
    establishes the base structure for future levels of the chatbot.

---
+++ src/main/java/Cate.java
@@ -0,0 +1,11 @@
+public class Cate {
+    public static void main(String[] args) {
+        String logo = "___________________________________________ \n"
+                + "Hello! I'm Cate \n"
+                + "What can I do for you? \n"
+                + "___________________________________________ \n"
+                + "Bye. Hope to see you again soon! \n"
+                + "___________________________________________ \n";
+        System.out.println(logo);
+    }
+}
