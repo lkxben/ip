8b0da08098342cd7d5223b9119e0f3ef0960b094
benlua <benlua73@gmail.com>
2025-08-19 00:19:32 +0800

    Add Level 1 echo feature to Cate
    Cate now echoes user input and exits only when 'bye' is entered.
    Horizontal lines separate messages for clarity in the console output.

--- src/main/java/Cate.java
+++ src/main/java/Cate.java
@@ -1,11 +1,22 @@
+import java.util.Scanner;
+
 public class Cate {
     public static void main(String[] args) {
-        String logo = "___________________________________________ \n"
-                + "Hello! I'm Cate \n"
-                + "What can I do for you? \n"
-                + "___________________________________________ \n"
-                + "Bye. Hope to see you again soon! \n"
-                + "___________________________________________ \n";
-        System.out.println(logo);
+        String line = "    _______________________________________ \n";
+        String startup = line
+                + "    Hello! I'm Cate \n"
+                + "    What can I do for you? \n"
+                + line;
+        System.out.println(startup);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equals("bye")) {
+                break;
+            }
+            System.out.println(line + "    " + userInput + "\n" + line);
+        }
+        System.out.println(line + "    Bye. Hope to see you again soon! \n" + line);
     }
 }
