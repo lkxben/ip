63dde24b32f2616271ae567beb0ad8fc88288f28
benlua <benlua73@gmail.com>
2025-08-19 14:19:06 +0800

    Add Level 2 task storage and listing to Cate
    Cate can now store user input as tasks and list them on command.
    Tasks are kept in memory with a fixed-size array of up to 100 items.

--- src/main/java/Cate.java
+++ src/main/java/Cate.java
@@ -9,14 +9,29 @@ public class Cate {
                 + line;
         System.out.println(startup);
         Scanner scanner = new Scanner(System.in);
+        String[] list = new String[100];
+        int index = 0;
 
         while (true) {
             String userInput = scanner.nextLine();
-            if (userInput.equals("bye")) {
+            if (userInput.equals("list")) {
+                System.out.print(line);
+                printList(list);
+                System.out.print(line);
+            } else if (userInput.equals("bye")) {
                 break;
+            } else {
+                System.out.println(line + "    added: " + userInput + "\n" + line);
+                list[index++] = userInput;
             }
-            System.out.println(line + "    " + userInput + "\n" + line);
         }
         System.out.println(line + "    Bye. Hope to see you again soon! \n" + line);
+    }
+
+    private static void printList(String[] list) {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] ==  null) return;
+            System.out.println(String.format("    %d. %s", i + 1, list[i]));
+        }
     }
 }
