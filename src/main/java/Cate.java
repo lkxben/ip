57ff1d184e1313c61548c931480229c4b00be111
benlua <benlua73@gmail.com>
2025-08-19 18:42:55 +0800

    Add support for deleting tasks from the list

--- src/main/java/Cate.java
+++ src/main/java/Cate.java
@@ -1,8 +1,8 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Cate {
-    private static final Task[] list = new Task[100];
-    private static int index = 0;
+    private static final ArrayList<Task> list = new ArrayList<>();
     private static final String line = "    _______________________________________\n";
 
     public static void main(String[] args) {
@@ -31,15 +31,17 @@ public class Cate {
         } else if (userInput.equals("todo")) {
             throw new CateException("The description of a todo cannot be empty.");
         } else if (userInput.startsWith("mark ")) {
-            String[] parts = userInput.split(" ");
-            String number = parts[1];
+            String number = userInput.split(" ")[1];
             int value = Integer.parseInt(number);
             markTask(value);
         } else if (userInput.startsWith("unmark ")) {
-            String[] parts = userInput.split(" ");
-            String number = parts[1];
+            String number = userInput.split(" ")[1];
             int value = Integer.parseInt(number);
             unmarkTask(value);
+        } else if (userInput.startsWith("delete ")) {
+            String number = userInput.split(" ")[1];
+            int value = Integer.parseInt(number);
+            deleteTask(value);
         } else if (userInput.startsWith("todo ")) {
             String description = userInput.split(" ", 2)[1];
             Task newTask = new Todo(description);
@@ -62,30 +64,40 @@ public class Cate {
 
     private static void addTask(Task task) {
         System.out.println(line + "    Got it. I've added this task:\n      " + task.toString());
-        list[index++] = task;
-        System.out.println("    Now you have " + index + " tasks in the list.\n" + line);
+        list.add(task);
+        System.out.println("    Now you have " + list.size() + " tasks in the list.\n" + line);
+    }
+
+    private static void deleteTask(int i) {
+        Task t = list.get(i - 1);
+        System.out.print(line);
+        System.out.println(String.format("    Noted. I've removed this task:\n      %s", t));
+        list.remove(t);
+        System.out.println("    Now you have " + list.size() + " tasks in the list.\n" + line);
     }
 
     private static void markTask(int i) {
-        list[i - 1].markDone();
+        Task t = list.get(i - 1);
+        t.markDone();
         System.out.print(line);
-        System.out.println(String.format("    Nice! I've marked this task as done:\n      %s", list[i - 1]));
+        System.out.println(String.format("    Nice! I've marked this task as done:\n      %s", t));
         System.out.println(line);
     }
 
     private static void unmarkTask(int i) {
-        list[i - 1].markUndone();
+        Task t = list.get(i - 1);
+        t.markUndone();
         System.out.print(line);
-        System.out.println(String.format("    OK, I've marked this task as not done yet:\n      %s", list[i - 1]));
+        System.out.println(String.format("    OK, I've marked this task as not done yet:\n      %s", t));
         System.out.println(line);
     }
 
     private static void printList() {
         System.out.print(line);
         System.out.println("    Here are the tasks in your list:");
-        for (int i = 0; i < list.length; i++) {
-            if (list[i] ==  null) break;
-            System.out.println(String.format("    %d. %s", i + 1, list[i]));
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) ==  null) break;
+            System.out.println(String.format("    %d. %s", i + 1, list.get(i)));
         }
         System.out.println(line);
     }
