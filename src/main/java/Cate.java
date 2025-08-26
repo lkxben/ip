830b2700ebc9a4b1e4adef5c0f4a618387f5bc8b
benlua <benlua73@gmail.com>
2025-08-26 20:37:28 +0800

    Extract tasklist class

--- src/main/java/Cate.java
+++ src/main/java/Cate.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 
 public class Cate {
     private static final String filePath = "src/main/data/cate.txt";
-    private static final ArrayList<Task> list = Storage.loadTask(filePath);
     private static final String line = "    _______________________________________\n";
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
 
     private Ui ui;
+    private TaskList tasks;
 
     public Cate(String filePath) {
         ui = new Ui();
+        tasks = new TaskList(Storage.loadTask(filePath));
     }
 
     public void run() {
@@ -35,83 +36,44 @@ public class Cate {
         new Cate(filePath).run();
     }
 
-    private static void processUserInput(String userInput) throws CateException {
+    private void processUserInput(String userInput) throws CateException {
         if (userInput.equals("list")) {
-            printList();
+            tasks.printList();
         } else if (userInput.equals("todo")) {
             throw new CateException("The description of a todo cannot be empty.");
         } else if (userInput.startsWith("mark ")) {
             String number = userInput.split(" ")[1];
             int value = Integer.parseInt(number);
-            markTask(value);
+            tasks.markTask(value);
         } else if (userInput.startsWith("unmark ")) {
             String number = userInput.split(" ")[1];
             int value = Integer.parseInt(number);
-            unmarkTask(value);
+            tasks.unmarkTask(value);
         } else if (userInput.startsWith("delete ")) {
             String number = userInput.split(" ")[1];
             int value = Integer.parseInt(number);
-            deleteTask(value);
+            tasks.deleteTask(value);
         } else if (userInput.startsWith("todo ")) {
             String description = userInput.split(" ", 2)[1];
             Task newTask = new Todo(description);
-            addTask(newTask);
+            tasks.addTask(newTask);
             Storage.saveTask(newTask, filePath);
         } else if (userInput.startsWith("deadline ")) {
             String input = userInput.split(" ", 2)[1];
             String[] parts = input.split(" /by ", 2);
             Task newTask = new Deadline(parts[0], LocalDateTime.parse(parts[1], formatter));
-            addTask(newTask);
+            tasks.addTask(newTask);
             Storage.saveTask(newTask, filePath);
         } else if (userInput.startsWith("event ")) {
             String input = userInput.split(" ", 2)[1];
             String[] parts = input.split(" /from ", 2);
             String[] time = parts[1].split(" /to ", 2);
             Task newTask = new Event(parts[0], time[0], time[1]);
-            addTask(newTask);
+            tasks.addTask(newTask);
             Storage.saveTask(newTask, filePath);
         } else {
             throw new CateException("I'm sorry I don't understand!");
         }
     }
 
-    private static void addTask(Task task) {
-        System.out.println(line + "    Got it. I've added this task:\n      " + task.toString());
-        list.add(task);
-        System.out.println("    Now you have " + list.size() + " tasks in the list.\n" + line);
-    }
-
-    private static void deleteTask(int i) {
-        Task t = list.get(i - 1);
-        System.out.print(line);
-        System.out.println(String.format("    Noted. I've removed this task:\n      %s", t));
-        list.remove(t);
-        System.out.println("    Now you have " + list.size() + " tasks in the list.\n" + line);
-    }
-
-    private static void markTask(int i) {
-        Task t = list.get(i - 1);
-        t.markDone();
-        System.out.print(line);
-        System.out.println(String.format("    Nice! I've marked this task as done:\n      %s", t));
-        System.out.println(line);
-    }
-
-    private static void unmarkTask(int i) {
-        Task t = list.get(i - 1);
-        t.markUndone();
-        System.out.print(line);
-        System.out.println(String.format("    OK, I've marked this task as not done yet:\n      %s", t));
-        System.out.println(line);
-    }
-
-    private static void printList() {
-        System.out.print(line);
-        System.out.println("    Here are the tasks in your list:");
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) ==  null) break;
-            System.out.println(String.format("    %d. %s", i + 1, list.get(i)));
-        }
-        System.out.println(line);
-    }
 }
